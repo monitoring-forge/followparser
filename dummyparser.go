d998251cd539e76d331f9c6eb357d0b9a3f64fb1
Masahiro Nagano <m-nagano@sakura.ad.jp>
2026-07-28 21:01:41 +0900

    add dummyParser comment

--- dummyparser.go
+++ dummyparser.go
@@ -1,10 +1,13 @@
 package followparser
 
+// dummyParser is a parser that does nothing. It is used for testing purposes.
 type dummyParser struct{}
 
 func (p *dummyParser) Parse(_ []byte) error {
+	// do nothing
 	return nil
 }
 
 func (p *dummyParser) Finish(_ float64) {
+	// do nothing
 }
